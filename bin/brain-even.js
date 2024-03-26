bd7ccd1ab53253b19c38a5e869c4a9ee4ca41232
Sh1Y4 <shitkin_i@mail.ru>
2024-03-26 18:52:35 +0300

    5.3 step of project

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -14,6 +14,13 @@ console.log('Answer \'yes\' if the number is even, otherwise answer \'no\'.');
 const isEven = () => {
 	const random = (min = +minNum, max = +maxNum) => Math.floor(Math.random() * (max - min + 1)) + min; // Создаем число
 	const randomNumber = random();
+	let correctAnswer = '';
+
+	if (randomNumber % 2 === 0) {
+		correctAnswer = 'yes';
+	} else {
+		correctAnswer = 'no';
+	}
 
 	const answer = readlineSync.question(`Question: ${randomNumber}\nYour answer: `);
 
@@ -25,10 +32,10 @@ const isEven = () => {
 		}
 		isEven();
 	} else if ((randomNumber % 2 !== 0 && answer === 'yes') || (randomNumber % 2 === 0 && answer === 'no')) {
-		console.log(`'${answer}' is wrong answer ;(. Correct answer was 'no'.
+		console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.
         Let's try again, ${name}`);
 	} else if (answer !== 'yes' && answer !== 'no') {
-		console.log(`'${answer}' is wrong answer ;(. Correct answer was 'no'.
+		console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.
         Let's try again, ${name}`);
 	}// Проверка ответа и счет правильных ответов
 };
