8cbe7e4ed6c4cff394ac5dc5d7c3ab3453e819e9
Sh1Y4 <shitkin_i@mail.ru>
2024-04-05 13:15:57 +0300

    9th refactoring

--- src/brain-calc-logic.js
+++ src/brain-calc-logic.js
@@ -10,30 +10,30 @@ let score = 0;
 
 // Игра "Калькулятор"
 const calc = () => {
-	const randomNumberFirst = random(1, 100);
-	const randomNumberSecond = random(1, 100);
-	const operatorSymbol = ['+', '-', '*'][random(1, 3) - 1];
-	let correctAnswer;
-	// eslint-disable-next-line default-case
-	switch (operatorSymbol) {
-	case '+':
-		correctAnswer = randomNumberFirst + randomNumberSecond;
-		break;
-	case '-':
-		correctAnswer = randomNumberFirst - randomNumberSecond;
-		break;
-	case '*':
-		correctAnswer = randomNumberFirst * randomNumberSecond;
-		break;
-	}
+  const randomNumberFirst = random(1, 100);
+  const randomNumberSecond = random(1, 100);
+  const operatorSymbol = ['+', '-', '*'][random(1, 3) - 1];
+  let correctAnswer;
+  // eslint-disable-next-line default-case
+  switch (operatorSymbol) {
+    case '+':
+      correctAnswer = randomNumberFirst + randomNumberSecond;
+      break;
+    case '-':
+      correctAnswer = randomNumberFirst - randomNumberSecond;
+      break;
+    case '*':
+      correctAnswer = randomNumberFirst * randomNumberSecond;
+      break;
+    }
 
-	if (score < 3) {
-		score += 1;
-		playGame(`Question: ${randomNumberFirst} ${operatorSymbol} ${randomNumberSecond}\nYour answer: `, correctAnswer);
-		calc();
-	} else {
-		console.log(`Congratulations, ${name}!`);
-	}
+    if (score < 3) {
+      score += 1;
+      playGame(`Question: ${randomNumberFirst} ${operatorSymbol} ${randomNumberSecond}\nYour answer: `, correctAnswer);
+      calc();
+    } else {
+      console.log(`Congratulations, ${name}!`);
+    }
 };
 
 calc();
